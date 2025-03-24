8aa1dfb3d8a818493c8afb62c2ba5bca9fdce445
Oleg Angulyarov <4nglrv@gmail.com>
2025-03-25 00:07:28 +0300

    feat: dark theme

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,10 +1,12 @@
 import styles from "./Header.module.css";
+import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
     <div className={styles.header}>
       <h1>CoSMI Chat</h1>
       <div className={styles.headerActions}>
+        <ThemeToggle />
         <button className={styles.headerButton}>Модель: GigaChat</button>
         <button className={styles.headerButton}>Очистить чат</button>
       </div>
